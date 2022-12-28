fe75c61e457d0f22aec163daeb1c99c78662122a
SHASHANK H M <113587615+ShashankHm91@users.noreply.github.com>
2022-12-28 21:32:42 +0530

    blog frontend added

---
+++ src/components/Auth.js
@@ -0,0 +1,100 @@
+import { Box, Button, TextField, Typography } from "@mui/material";
+import React, { useState } from "react";
+import axios from "axios";
+import { useDispatch } from "react-redux";
+import { authActions } from "../store";
+import { useNavigate } from "react-router-dom";
+import "./auth.css"
+
+const Auth = () => {
+  const naviagte = useNavigate();
+  const dispath = useDispatch();
+  const [inputs, setInputs] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+  const [isSignup, setIsSignup] = useState(false);
+  const handleChange = (e) => {
+    setInputs((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value,
+    }));
+  };
+  const sendRequest = async (type = "login") => {
+    const res = await axios
+      .post(`https://blogbackend-izxb.onrender.com/api/user/${type}`, {
+        name: inputs.name,
+        email: inputs.email,
+        password: inputs.password,
+      })
+      .catch((err) => console.log(err));
+
+    const data = await res.data;
+    console.log(data);
+    return data;
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(inputs);
+    if (isSignup) {
+      sendRequest("signup")
+        .then((data) => localStorage.setItem("userId", data.user._id))
+        .then(() => dispath(authActions.login()))
+        .then(() => naviagte("/blogs"));
+    } else {
+      sendRequest()
+        .then((data) => localStorage.setItem("userId", data.user._id))
+        .then(() => dispath(authActions.login()))
+        .then(() => naviagte("/blogs"));
+    }
+  };
+  return (
+    <div>
+      <form onSubmit={handleSubmit} className="formpage">
+       
+          <h2  className="cont">
+            {isSignup ? "Signup" : "Login"}
+          </h2>
+          {isSignup 
+            
+          }{" "}
+          <TextField
+            name="email"
+            onChange={handleChange}
+            value={inputs.email}
+            type={"email"}
+            placeholder="Email"
+            margin="normal"
+          />
+          <TextField
+            name="password"
+            onChange={handleChange}
+            value={inputs.password}
+            type={"password"}
+            placeholder="Password"
+            margin="normal"
+          />
+
+          
+          <Button
+            type="submit"
+            variant="contained"
+            sx={{ borderRadius: 3, marginTop: 3 }}
+            color="warning"
+          >
+            Submit
+          </Button>
+          <Button
+            onClick={() => setIsSignup(!isSignup)}
+            sx={{ borderRadius: 3, marginTop: 3 }}
+          >
+            Change To {isSignup ? "Login" : "Signup"}
+          </Button>
+      </form>
+    </div>
+  );
+};
+
+export default Auth;
